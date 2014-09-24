a1a5df8cf43db99e4b3a33dadd70dbdda8dfd793
kingdon <kinpauln@126.com>
2014-09-24 23:54:43 +0800

    q

--- Src/LotteryDraw.Site.Models/LoginModel.cs
+++ Src/LotteryDraw.Site.Models/LoginModel.cs
@@ -16,14 +16,14 @@ namespace LotteryDraw.Site.Models
         /// <summary>
         /// 获取或设置 登录账号
         /// </summary>
-        [Required]
+        [Required(ErrorMessage="{0}不能为空！")]
         [Display(Name = "登录账号")]
         public string Account { get; set; }
 
         /// <summary>
         /// 获取或设置 登录密码
         /// </summary>
-        [Required]
+        [Required(ErrorMessage = "{0}不能为空！")]
         [DataType(DataType.Password)]
         [Display(Name = "登录密码")]
         public string Password { get; set; }
--- Src/LotteryDraw.Site.Models/MemberView.cs
+++ Src/LotteryDraw.Site.Models/MemberView.cs
@@ -1,6 +1,7 @@
 ﻿using LotteryDraw.Component.Tools;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 
@@ -10,10 +11,14 @@ namespace LotteryDraw.Site.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "{0}不能为空！")]
+        [Display(Name = "登录账号")]
         public string UserName { get; set; }
 
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0}不能为空！")]
+        [Display(Name = "登录密码")]
         public string Password { get; set; }
 
         public string Email { get; set; }
